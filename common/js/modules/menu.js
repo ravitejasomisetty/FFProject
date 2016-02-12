74757478dba8a33c62972977b1efe345e5ac8bdc
Vinod Reddy <vinod492@gmail.com>
2016-02-11 19:23:10 -0700

    latest

--- common/js/modules/menu.js
+++ common/js/modules/menu.js
@@ -1,8 +1,21 @@
+
+
 var app = angular.module('myapp', []);
 
-app.directive('ffMenu', function() {
-  return {
-      restrict: 'E',
-      templateUrl: '../common/js/modules/partials/menu.tpl.html'
-   };
-});
+
+       app.directive('ffMenu', function() {
+          return {
+              restrict: 'E',
+              templateUrl: '../common/js/modules/partials/menu.tpl.html',
+              controller: ['$scope',function($scope){
+
+
+                  $scope.menuItems = [{"id":"home","class":"active","title":"Home"},
+                                   {"id":"profile","class":"profile","title":"Profile"},
+                                   {"id":"admin","class":"admin","title":"Admin"},
+                                   {"id":"forms","class":"forms","title":"Forms"}];
+
+
+                }],
+            };
+   });
