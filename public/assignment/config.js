e18fd5db31ad0d7977f52de38d380b74b6023446
Vinod Reddy <vinod492@gmail.com>
2016-02-20 11:49:12 -0700

    commit with errors

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -1,1 +1,27 @@
 
+(function(){
+    angular
+        .module("config", ["ngRoute"])
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/", {
+                    templateUrl: "/views/users/home.view.html"
+                })
+                .when("/register", {
+                    templateUrl: "/views/users/register.view.html"
+                })
+                .when("/login", {
+                    templateUrl: "/views/users/login.view.html"
+                })
+                .when("/profile", {
+                    templateUrl: "/views/users/profile.view.html"
+                })
+                .when("/admin", {
+                    templateUrl: "/views/users/admin.view.html"
+                })
+
+                .otherwise({
+                    redirectTo: "/views/users/home.view.html"
+                });
+        });
+})();
