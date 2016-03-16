0205b6995eccdbead94a20c6fb880e86a9c4e12e
Raviteja Somisetty <somisetty.r@husky.neu.edu>
2016-03-15 21:27:57 -0400

    models and user.service.server

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 app.listen(port, ipaddress);
+
